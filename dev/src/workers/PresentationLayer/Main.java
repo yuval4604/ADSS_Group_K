ab206eda4384608959dff348836d5fe5e36ba2a9
yuval4604 <yuvalzar@post.bgu.ac.il>
2024-05-27 21:56:05 +0300

    Update Main.java

--- dev/src/workers/PresentationLayer/Main.java
+++ dev/src/workers/PresentationLayer/Main.java
@@ -26,7 +26,7 @@ public class Main {
                 }
             }
             else{
-                System.out.println("Choose command:\n1) logout\n2) change password\n3) Show your information\n4) Show your prefrences \n5) Set bank number\n6) use vacation days\n7) set your prefrences\nHR Only Commands:\n8) Set a worker's global wage\n9) Set a worker's hourly wage\n10) Set if a worker is full time job employee or not\n11) Set a worker's vacation days\n12) Reset a worker's vacation days\n13) add a worker\n14) work on shift\n15) change a worker's optional roles");
+                System.out.println("Choose command:\n1) logout\n2) change password\n3) Show your information\n4) Show your prefrences \n5) Set bank number\n6) use vacation days\n7) set your prefrences\nHR Only Commands:\n8) Set a worker's global wage\n9) Set a worker's hourly wage\n10) Set if a worker is full time job employee or not\n11) Set a worker's vacation days\n12) Reset a worker's vacation days\n13) add a worker\n14) work on shift\n15) change a worker's optional roles\n16) set half a day off\n17) set a full day off");
                 int result = scanner.nextInt(); // TODO try & catch
 
                 switch(result) {
