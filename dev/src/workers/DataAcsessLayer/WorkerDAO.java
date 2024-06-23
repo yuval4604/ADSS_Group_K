3fbd315091b285e4620e4b4b9b0f2eda0b8d705f
yuval4604 <yuvalzar@post.bgu.ac.il>
2024-06-23 23:11:35 +0300

    Update WorkerDAO.java

--- dev/src/workers/DataAcsessLayer/WorkerDAO.java
+++ dev/src/workers/DataAcsessLayer/WorkerDAO.java
@@ -1,13 +1,47 @@
 package workers.DataAcsessLayer;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class WorkerDAO  {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/workerdb";
+
+    public static void createWorkerTable() {
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            System.out.println("Creating table in given database...");
+            String sql = "CREATE TABLE Registration " +
+                    "(id INTEGER not NULL, " +
+                    " first VARCHAR(255), " +
+                    " last VARCHAR(255), " +
+                    " age INTEGER, " +
+                    " PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql);
+            System.out.println("Created table in given database...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void addWorker(WorkerDTO worker) {
-        
-
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            System.out.println("Inserting records into the table...");
+            String sql = "INSERT INTO Registration VALUES (100, 'Zara', 'Ali', 18)";
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Registration VALUES (101, 'Mahnaz', 'Fatma', 25)";
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Registration VALUES (102, 'Zaid', 'Khan', 30)";
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Registration VALUES(103, 'Sumit', 'Mittal', 28)";
+            stmt.executeUpdate(sql);
+            System.out.println("Inserted records into the table...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+}
