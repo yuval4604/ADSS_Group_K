fe228a5b5313812a7f58304788632cef06349421
yuval4604 <yuvalzar@post.bgu.ac.il>
2024-06-23 23:55:32 +0300

    create and insert in workerDAO+WORKERDTO

--- dev/src/workers/DataAcsessLayer/WorkerDAO.java
+++ dev/src/workers/DataAcsessLayer/WorkerDAO.java
@@ -9,16 +9,38 @@ public class WorkerDAO  {
         try (Connection conn = DriverManager.getConnection(DB_URL);
              Statement stmt = conn.createStatement();
         ) {
-            // Execute a query
-            System.out.println("Creating table in given database...");
-            String sql = "CREATE TABLE Registration " +
-                    "(id INTEGER not NULL, " +
-                    " first VARCHAR(255), " +
-                    " last VARCHAR(255), " +
-                    " age INTEGER, " +
+
+            String sql = "CREATE TABLE Workers " +
+                    "(ID INTEGER not NULL, " +
+                    " Name VARCHAR(255), " +
+                    " BankNumber INTEGER " +
+                    " GWage INTEGER, " +
+                    " HWage INTEGER, " +
+                    " DateOfStart DATE, " +
+                    " FullTime BIT(1), " +
+                    " TotalVacationDays INTEGER, " +
+                    " CurrentVacationDays INTEGER, " +
+                    " isBM BIT(1), " +
+                    " Changed BIT(1), " +
+                    " CurrPref SET(14), " +
                     " PRIMARY KEY ( id ))";
             stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");
+            String sql2 = "CREATE TABLE Prefs " +
+                    "(ID INTEGER not NULL," +
+                    "SUNDAY BIT(1)," +
+                    "MONDAY BIT(1)," +
+                    "TUEDAY BIT(1)," +
+                    "WEDDAY BIT(1)," +
+                    "THURDAY BIT(1)," +
+                    "FRIDAY BIT(1)," +
+                    "SUNNIGHT BIT(1)," +
+                    "MONNIGHT BIT(1)," +
+                    "TUENIGHT BIT(1)," +
+                    "WEDNIGHT BIT(1)," +
+                    "THURNIGHT BIT(1)," +
+                    "FRINIGHT BIT(1)," +
+                    "                     PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -28,17 +50,12 @@ public class WorkerDAO  {
         try (Connection conn = DriverManager.getConnection(DB_URL);
              Statement stmt = conn.createStatement();
         ) {
-            // Execute a query
-            System.out.println("Inserting records into the table...");
-            String sql = "INSERT INTO Registration VALUES (100, 'Zara', 'Ali', 18)";
+            String sql = "INSERT INTO Workers (ID, Name, BankNumber, GWage, HWage, DateOfStart, FullTime, TotalVacationDays, CurrentVacationDays, isBM, Changed, CurrPref) " +
+                    "VALUES (" + worker.getID() + ", '" + worker.getName() + "', " + worker.getBankAccount() + ", " + worker.getGWage() + ", " + worker.getHWage() + ", '" + worker.getStartDate() + "', " + worker.getFTime() + ", " + worker.getTVDays() + ", " + worker.getCVDays() + ", " + worker.getIsHeadOfBranch() + ", " + worker.getChange() + ", " + ")";
             stmt.executeUpdate(sql);
-            sql = "INSERT INTO Registration VALUES (101, 'Mahnaz', 'Fatma', 25)";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Registration VALUES (102, 'Zaid', 'Khan', 30)";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Registration VALUES(103, 'Sumit', 'Mittal', 28)";
-            stmt.executeUpdate(sql);
-            System.out.println("Inserted records into the table...");
+            String sql2 = "INSERT INTO Prefs (ID, SUNDAY, MONDAY, TUEDAY, WEDDAY, THURDAY, FRIDAY, SUNNIGHT, MONNIGHT, TUENIGHT, WEDNIGHT, THURNIGHT, FRINIGHT) " +
+                    "VALUES (" + worker.getID() + ", " + worker.getPref().get("SUNDAY") + ", " + worker.getPref().get("MONDAY") + ", " + worker.getPref().get("TUEDAY") + ", " + worker.getPref().get("WEDDAY") + ", " + worker.getPref()[4][0] + ", " + worker.getPref()[5][0] + ", " + worker.getPref()[0][1] + ", " + worker.getPref()[1][1] + ", " + worker.getPref()[2][1] + ", " + worker.getPref()[3][1] + ", " + worker.getPref()[4][1] + ", " + worker.getPref()[5][1] + ")";
+                    stmt.executeUpdate(sql2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- dev/src/workers/DataAcsessLayer/WorkerDTO.java
+++ dev/src/workers/DataAcsessLayer/WorkerDTO.java
@@ -1,4 +1,100 @@
 package workers.DataAcsessLayer;
 
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
 public class WorkerDTO {
+    private int id;
+    private String name;
+    private int bankAccount;
+    private int HWage;
+    private int GWage;
+    private LocalDate startDate;
+    private boolean FTime;
+    private int TVDays;
+    private int CVDays;
+    private String branchName;
+    private boolean isHeadOfBranch;
+    private boolean change;
+    private String[][] pref;
+    public int getID() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getBankAccount() {
+        return bankAccount;
+    }
+    public int getHWage() {
+        return HWage;
+    }
+    public int getGWage() {
+        return GWage;
+    }
+    public String getStartDate() {
+        return startDate.getDayOfMonth() + "/" + startDate.getMonthValue() + "/" + startDate.getYear();
+    }
+    public boolean getFTime() {
+        return FTime;
+    }
+    public int getTVDays() {
+        return TVDays;
+    }
+    public int getCVDays() {
+        return CVDays;
+    }
+    public String getBranchName() {
+        return branchName;
+    }
+    public boolean getIsHeadOfBranch() {
+        return isHeadOfBranch;
+    }
+    public boolean getChange() {
+        return change;
+    }
+    public void setID(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setBankAccount(int bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+    public void setHWage(int HWage) {
+        this.HWage = HWage;
+    }
+    public void setGWage(int GWage) {
+        this.GWage = GWage;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public void setFTime(boolean FTime) {
+        this.FTime = FTime;
+    }
+    public void setTVDays(int TVDays) {
+        this.TVDays = TVDays;
+    }
+    public void setCVDays(int CVDays) {
+        this.CVDays = CVDays;
+    }
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+    public void setIsHeadOfBranch(boolean isHeadOfBranch) {
+        this.isHeadOfBranch = isHeadOfBranch;
+    }
+    public void setChange(boolean change) {
+        this.change = change;
+    }
+
+    public String[][] getPref() {
+        return pref;
+    }
+
+    public void setPref(String[][] pref) {
+        this.pref = pref;
+    }
 }
